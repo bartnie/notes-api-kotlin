c30a241d1eb66489622b9fdb4669bc388fbd12df
bniesobski <bartosz.niesobski@gmail.com>
2021-10-14 21:18:57 +0200

    project generated with spring initializer

---
+++ src/main/kotlin/pl/bartek/notes/notesapi/NotesApiApplication.kt
@@ -0,0 +1,11 @@
+package pl.bartek.notes.notesapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotesApiApplication>(*args)
+}
---
+++ src/test/kotlin/pl/bartek/notes/notesapi/NotesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.bartek.notes.notesapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
